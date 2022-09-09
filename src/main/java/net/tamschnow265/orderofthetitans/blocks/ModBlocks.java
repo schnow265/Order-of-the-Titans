55070b78c81fc69806b0edbfea66315e7b47767d
TamSchnow265 <TamSchnow265@proton.me>
2022-09-09 20:31:56 +0200

    sig();

--- src/main/java/net/tamschnow265/orderofthetitans/blocks/ModBlocks.java
+++ src/main/java/net/tamschnow265/orderofthetitans/blocks/ModBlocks.java
@@ -3,17 +3,16 @@ package net.tamschnow265.orderofthetitans.blocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
-import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.tamschnow265.orderofthetitans.item.ModItemGroup;
 import net.tamschnow265.orderofthetitans.modloader;
+import net.minecraft.block.OreBlock;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block URANIUM_BLOCK = registerBlock("uranium_block",
